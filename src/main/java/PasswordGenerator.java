800cc415763a0239279c5f4d501cbef8013e83a8
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-03-21 13:39:03 +0200

    Added a method to return random password

--- src/main/java/PasswordGenerator.java
+++ src/main/java/PasswordGenerator.java
@@ -1,5 +1,39 @@
+/**
+ * Task: Random Password Generator
+ *
+ * Description: Build a program that generates a
+ * random password for the user. Prompt the user to
+ * enter the desired length of the password and
+ * specify whether it should include numbers,
+ * lowercase letters, uppercase letters, and special
+ * characters. Generate the password accordingly
+ * and display it to the user.
+ *
+ * Skills: Random number generation,
+ */
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+
 public  class PasswordGenerator  {
   public static void main(String[] args) {
-        System.out.println("Password Genrator");
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Enter desired length:");
+
+      int desired_length = scanner.nextInt();
+      System.out.println("Should it include Special char?: <Y/N>");
+      String userResposne =  scanner.nextLine().toUpperCase().trim();
+      if(userResposne.equals("Y")){
+          // return password with special charecters
+          String randomPassword = passwordWithSpecialCharecters();
+          System.out.println(randomPassword);
+
+      }
+
+    }
+    static  String passwordWithSpecialCharecters(){
+
     }
 }
